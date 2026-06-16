eaee983dd096d9bfdd06dd124fb3440d70c287b8
luisricarte <luis.ricarte@ccc.ufcg.edu.br>
2026-06-15 22:29:31 -0300

    adiciona configuracao ssl

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,8 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLConnection(),
   });
-
-  console.log("Executing query:",{
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  } );
 
   try{  
   await client.connect();
@@ -31,6 +23,13 @@ async function query(queryObject) {
   await client.end();
   }
 
+}
+
+function getSSLConnection() {
+  if(process.env.POSTGRES_CA) {
+    return {ca: process.env.POSTGRES_CA};
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 module.exports = { query: query };
